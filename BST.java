e8a115190a3e58e1b13f721cef2085c3f0e00a6e
Prateek Chouhan <42679687+Prateek9144@users.noreply.github.com>
2019-04-04 00:09:37 +0530

    Update BST.java

--- BST.java
+++ BST.java
@@ -45,12 +45,77 @@ class BST<D>{
         postTraverse(r.right);
         System.out.print(r.val+"\t");
     }
+      private TreeNode search(Integer val, TreeNode<Integer> rt)
+    {
+        if(val==rt.val)
+        return rt;
+        else if(val<rt.val)
+        {
+            if(rt.left!=null)
+                return search(val, rt.left);
+            else
+                return null;
+        }
+        else
+        {
+            if(rt.right!=null)
+                return search(val, rt.right);
+            else
+                return null;
+        }
+    }
+    public void search(Integer val)
+    {
+        TreeNode x=search(val, root);
+        if(x==null)
+        System.out.println("Element not found");
+        else
+        System.out.println("Element Found");
+    }
     public void preTraverse(Node r){
         if(r==null)
             return ;
         System.out.print(r.val+"\t");
         preTraverse(r.left);
         preTraverse(r.right);
+    }
+     public void delete(Integer val)
+    {
+        TreeNode x=search(val, root);
+        if(x==null)
+        throw new NoSuchElementException();
+        else
+        {
+            if(x.right==null && x.left==null)
+            x=null;
+            else if(x.right==null && x.left!=null)
+            {
+                TreeNode parent=findParent(x,root);
+                if(parent.right==x)
+                parent.right=x.left;
+                else
+                parent.left=x.left;
+            }
+            else if(x.left==null && x.right!=null)
+            {
+                TreeNode parent=findParent(x,root);
+                if(parent.right==x)
+                parent.right=x.right;
+                else
+                parent.left=x.right;
+            }
+            else
+            {
+                TreeNode<Integer> par=x.left;
+                while(par.right!=null)
+                {
+                    par=par.right;
+                }
+                int temp=par.val;
+                delete(par.val);
+                x.val=temp;
+            }
+        }
     }
   
     public static void main(String[] args){
